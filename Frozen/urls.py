0a574a61e8c9766161c45e45565adaa15b5e09dc
JacobSamarita <jakersam62@gmail.com>
2019-12-29 01:37:08 +0400

    a

--- Frozen/urls.py
+++ Frozen/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 app_name = 'Frozen'
 
 urlpatterns = [
     path('',views.Home, name='Home')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
