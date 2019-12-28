8fd4c5508af1996747de67a645ba99530afb083f
JacobSamarita <jakersam62@gmail.com>
2019-12-28 23:39:57 +0400

    debug true

--- KabayanStore/settings.py
+++ KabayanStore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n1a!#!hss%o5k&bvng4zxhfj=dz_dijzdojy!-)va(lf4hx@u7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'kabayansohar.pythonanywhere.com',
